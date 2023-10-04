f324f454cff823e2338f286cf6eb031f70868d6d
vallabh <dvallabh007@gmail.com>
2023-10-04 12:53:26 +0530

    modified files

--- task/src/app/content/content.component.ts
+++ task/src/app/content/content.component.ts
@@ -1,38 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
   formData!: FormGroup;
   constructor(private fb: FormBuilder) {
     this.formData = this.fb.group({
-      productinfo: this.fb.group({
-        quoteNumber: ['', Validators.required],
-        location: ['', Validators.required],
-        eConsumption: ['', Validators.required],
-        billInfo: ['', Validators.required],
-        avaiRoofSpace: ['', Validators.required],
-        bStorage: ['', Validators.required],
-      }),
-      productBillInfo:this.fb.array([this.billingFilds])
+      quoteNumber: ['', Validators.required],
+      location: ['', Validators.required],
+      eConsumption: ['', Validators.required],
+      billInfo: ['', Validators.required],
+      avaiRoofSpace: ['', Validators.required],
+      bStorage: ['', Validators.required],
+      productBillInfo: this.fb.array([this.billingFilds()]),
     });
   }
-  billingFilds(){
+  ngOnInit(): void {}
+  billingFilds() {
     return this.fb.group({
-      item:['',Validators.required],
-      unitPrice:['',Validators.required],
-      quantity:['',Validators.required],
-      discount:['',Validators.required],
-      amount:['',Validators.required],
-    })
+      item: ['', Validators.required],
+      unitPrice: ['', Validators.required],
+      quantity: ['', Validators.required],
+      discount: ['', Validators.required],
+      amount: ['', Validators.required],
+    });
   }
-  get productBillInfoArray(){
+  get productBillInfoArray() {
     return this.formData.get('productBillInfo') as FormArray;
   }
-  addProduct(){
+  addProduct() {
     this.productBillInfoArray.push(this.billingFilds());
   }
 }
